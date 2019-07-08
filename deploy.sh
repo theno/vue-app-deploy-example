9784fc705b9f359d933e2ea820966e5bdc1ca5be
Theodor Nolte <nolte@dfn-cert.de>
2019-07-08 11:43:29 +0200

    fix git repo url in deploy script

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git commit -m 'deploy'
 
 # if you are deploying to
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:theno/vue-app-deploy-example/.git master:gh-pages
+git push -f git@github.com:theno/vue-app-deploy-example.git master:gh-pages
 
 cd -
